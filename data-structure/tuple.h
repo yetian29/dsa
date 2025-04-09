54fdb7a963c1427b84f633557640bcd1bfe1c3e2
yetian29 <you@example.comr>
2025-04-09 09:48:45 +0700

    Initial

---
+++ data-structure/tuple.h
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+#ifndef TUPLE_H
+#define TUPLE_H
+
+typedef struct Tuple Tuple;
+
+// Define implement function
+Tuple initialize(int a, int b, int c);
+bool comparation(Tuple t1, Tuple t2);
+
+
+
+
+#endif
+
+
