8c18a69b0d1f35f1d330ebde78f29cafe902ced5
yetian29 <you@example.comr>
2025-04-11 08:20:11 +0700

    Initial

--- data-structure/array.c
+++ data-structure/array.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
 
+// One dimentional array
+/*int main(void)*/
+/*{*/
+/*	int array[] = {1, 2, 3, 4, 5, 6};*/
+/*	int sizeArray = sizeof(array) / sizeof(array[0]);*/
+/*	printf("Size of array: %d\n", sizeArray);*/
+/*	for (int i = 0; i < sizeArray; i++)*/
+/*	{*/
+/*		printf("%p\n", (char *)array + (sizeof(int) * i));*/
+/*	}*/
+/**/
+/*}*/
+/**/
+
+// Two dimentional array
 int main(void)
 {
-	int array[] = {1, 2, 3, 4, 5, 6};
-	int sizeArray = sizeof(array) / sizeof(array[0]);
-	printf("Size of array: %d\n", sizeArray);
-	for (int i = 0; i < sizeArray; i++)
+	int array[2][3] = {1, 2, 3, 4, 5, 6};
+	for (int i = 0; i < 2; i++)
 	{
-		printf("%p\n", (char *)array + (sizeof(int) * i));
+		for (int j = 0; j < 3; j++)
+		{
+			printf("%d\n", array[i][j]);
+			printf("%p\n", (char *) array + (sizeof(int) * i) + (sizeof(int) * j));
+		}
 	}
-
 	return 0;
 }
+
